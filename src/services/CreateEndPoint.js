b98d6d2c2baca8efe3f9529e79520ad6c52d76c0
Connie <sitwailan@gmail.com>
2022-11-13 21:08:39 -0500

    fixed bugs

--- src/services/CreateEndPoint.js
+++ src/services/CreateEndPoint.js
@@ -24,27 +24,7 @@ export const createEndpoint = (endpoint) => {
         fetchByName: (name) => axios.get(BASE_URL + endpoint + "?name=" + name),
       };
       break;
-    // return {
-    //   fetchAll: () => axios.get(url),
-    //   fetchByName: (name) => axios.get(BASE_URL + endpoint + "?name=" + name),
-    //   fetchById: (id) => axios.get(url + id),
-    //   create: (newRecord) => axios.post(url, newRecord),
-    //   update: (id, updateRecord) => axios.put(url + id, updateRecord),
-    //   delete: (id) => axios.delete(url + id),
-    // };
     case ENDPOINTS.ORDER:
-      // return {
-      //   fetchAll: () => axios.get(url),
-      //   fetchByName: (name) => axios.get(BASE_URL + endpoint + "?name=" + name),
-      //   fetchById: (id) => axios.get(url + id),
-      //   create: (newRecord) => axios.post(url, newRecord),
-      //   update: (id, updateRecord) => axios.put(url + id, updateRecord),
-      //   delete: (id) => axios.delete(url + id),
-      //   fetchByIdAndDateRange: (id, fromDate, toDate) =>
-      //     axios.get(
-      //       BASE_URL + id + "/" + endpoint + "/" + fromDate + "/" + toDate
-      //     ),
-      // };
       api = {
         ...api,
         fetchByIdAndDateRange: (id, fromDate, toDate) =>
@@ -60,26 +40,8 @@ export const createEndpoint = (endpoint) => {
           axios.get(BASE_URL + category + "/" + endpoint),
       };
       break;
-    // return {
-    //   fetchAll: () => axios.get(url),
-    //   fetchAllByCategory: (category) =>
-    //     axios.get(BASE_URL + category + "/" + endpoint),
-    //   fetchById: (id) => axios.get(url + id),
-    //   create: (newRecord) => axios.post(url, newRecord),
-    //   update: (id, updateRecord) => axios.put(url + id, updateRecord),
-    //   delete: (id) => axios.delete(url + id),
-    // };
     default:
       break;
-    // return {
-    //   fetchAll: () => axios.get(url),
-    //   fetchAllByCategory: (category) =>
-    //     axios.get(BASE_URL + category + "/" + endpoint),
-    //   fetchById: (id) => axios.get(url + id),
-    //   create: (newRecord) => axios.post(url, newRecord),
-    //   update: (id, updateRecord) => axios.put(url + id, updateRecord),
-    //   delete: (id) => axios.delete(url + id),
-    // };
   }
 
   return api;
